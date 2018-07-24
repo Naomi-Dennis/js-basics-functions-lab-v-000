45f6ff83e1e0620ce20e8337f0e12481fe160bd3
Naomi Dennis <njdennis94@gmail.com>
2018-07-24 16:35:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 const BLOCK_IN_FEET = 43
 function distanceFromHqInBlocks(distance){
-  return distance / BLOCK_IN_FEET
+  return distance - BLOCK_IN_FEET + 1
 }
 
 function distanceFromHqInFeet(distance){

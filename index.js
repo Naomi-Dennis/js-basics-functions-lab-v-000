108473a958eb6464858586bbfb5093bc99ce1d6f
Naomi Dennis <njdennis94@gmail.com>
2018-07-24 16:30:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file!
+const blocks_in_feet = 43
 function distanceFromHqInBlocks(){
 
 }
@@ -12,5 +13,5 @@ function distanceTravelledInFeet(){
 }
 
 function calculatesFarePrice(start, destination){
-  
+
 }
